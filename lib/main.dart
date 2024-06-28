815faa4b92599b55140a80a23854b7055074a836
jpalman <john.palman.20@usjr.edu.ph>
2024-06-28 23:09:53 +0800

    Update Palman
    first update Luke.

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:state_change_demo/src/controllers/auth_controller.dart';
+import 'package:state_change_demo/src/routing/router.dart';
+
+void main() async{
+  AuthController.initialize();
+  GlobalRouter.initialize();
+  await AuthController.I.loadSession();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: GlobalRouter.I.router,
+      title: 'Personal Wellness Tracker App Sample',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
